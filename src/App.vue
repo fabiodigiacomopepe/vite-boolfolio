2c1fd2caa9ae8760720a0864a510a7730db19d9f
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-08-21 16:10:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import { store } from './store.js'
+import axios from 'axios'
+import AppHelloWorld from './components/AppHelloWorld.vue'
+
+
+export default {
+  components: {
+    AppHelloWorld,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+
+  },
+  created() {
+
+  }
+}
+</script>
+
+<template>
+  <AppHelloWorld />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+@use './styles/partials/mixins.scss' as *;
+</style>
---
+++ src/components/AppHelloWorld.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "AppHelloWorld",
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
+</style>
